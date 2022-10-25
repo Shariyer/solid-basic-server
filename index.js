8cbcf1adf87077f0374a3999749c57b7eba269fb
shariyer <s.m.shariyer04@gmail.com>
2022-10-25 12:40:18 +0600

    cors setup complete

--- index.js
+++ index.js
@@ -5,9 +5,15 @@ const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 5000;
 
+//cross origin resource sharing set up at app
+app.use(cors());
+
+// server running response in root
 app.get("/", (req, res) => {
   res.send("Congratulations!! server is Running");
 });
+
+// app listening at port 5000
 app.listen(port, (req, res) => {
   console.log("server is running at port: ", port);
 });
