41eddf14eaabe4c66719ffbabfefc775430c682a
shariyer <s.m.shariyer04@gmail.com>
2022-10-25 12:36:25 +0600

    app.listening added at port and console logged

--- index.js
+++ index.js
@@ -8,3 +8,6 @@ const port = process.env.PORT || 5000;
 app.get("/", (req, res) => {
   res.send("Congratulations!! server is Running");
 });
+app.listen(port, (req, res) => {
+  console.log("server is running at port: ", port);
+});
