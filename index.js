39c2223343ca96c9f4efd1cf25f7afc9ccf6fbeb
shariyer <s.m.shariyer04@gmail.com>
2022-10-25 12:34:57 +0600

    server response send in root

--- index.js
+++ index.js
@@ -4,3 +4,7 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 5000;
+
+app.get("/", (req, res) => {
+  res.send("Congratulations!! server is Running");
+});
